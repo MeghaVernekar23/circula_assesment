717f92c036392a5fda5c8459e8190f0b2d5f41b7
MeghaVernekar23 <meghavernekar98@gmail.com>
2025-02-06 15:47:29 +0100

    Commiting Base class file

---
+++ page-objects/base.js
@@ -0,0 +1,50 @@
+
+import dotenv from 'dotenv';
+
+export default class Base {
+
+    constructor(page, logFunction) {
+        if (!page) throw new Error("Page instance is required for Base class.");
+        this.page = page;
+        this.baseUrl = process.env.BASE_URL;
+        this.log = logFunction || console.log; 
+    }
+
+    async click(object) {
+        await this.getLocator(object).click(object["actOptions"]);
+        this.log(`Clicked on the "${object["description"]}" button`);
+    }
+
+    async forceClick(object) {
+        await this.getLocator(object).click({ force: true });
+        this.log(`Clicked on the "${object["description"]}" button`);
+    }
+
+    
+    async type(object, text) {
+        await this.getLocator(object).fill(text);
+        this.log(`Entered value "${text}" on "${object["description"]}"`);
+    }
+
+    
+    async getText(object) {
+        const text = await this.getLocator(object).innerText();
+        this.log(`Extracted text "${text}" from "${object["description"]}"`);
+        return text;
+    }
+
+    async visible(object) {
+        const isVisible = await this.getLocator(object).isVisible();
+        this.log(`"${object["description"]}" is ${isVisible ? "visible" : "not visible"}`);
+        return isVisible;
+    }
+
+    async navigate(path = '') {
+        await this.page.goto(`${this.baseUrl}${path}`);
+        this.log(`Navigated to: ${this.baseUrl}${path}`);
+    }
+
+    getLocator(object) {
+        return this.page.locator(object["locator"], object["locatorOptions"]);
+    }
+}
